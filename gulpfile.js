deaf4a3d4dc9c43daac7e9a85f76d82f519cb457
dfox <dfox@foxartbox.com>
2023-10-31 21:41:17 +0200

    base. Part 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,46 @@
-const fs            = require('fs');
-const concat        = require('gulp-concat')
-const config        = JSON.parse(fs.readFileSync('../config.json'))
-const cssMinify     = require('gulp-csso')
-const ftp           = require('vinyl-ftp')
-const gulp          = require('gulp')
-const gutil         = require('gulp-util')
-const rename        = require('gulp-rename')
-const sass          = require('gulp-sass')(require('sass'))
-const uglify        = require('gulp-uglify')
+const fs = require('fs');
+const concat = require('gulp-concat')
+const config = JSON.parse(fs.readFileSync('../config.json'))
+const cssMinify = require('gulp-csso')
+const ftp = require('vinyl-ftp')
+const gulp = require('gulp')
+const gutil = require('gulp-util')
+const rename = require('gulp-rename')
+const sass = require('gulp-sass')(require('sass'))
+const uglify = require('gulp-uglify')
 
 // FTP config
-const host          = config.host
-const password      = config.password
-const port          = config.port
-const user          = config.user
+const host = config.host
+const password = config.password
+const port = config.port
+const user = config.user
 
-const remoteFolder          = '/foxartbox.com/public_html/t2/'
-const remoteCss             = remoteFolder + 'css/'
-const remoteJs              = remoteFolder + 'js/'
-const remoteTemplateParts   = remoteFolder + 'template-parts/'
+const remoteFolder = '/foxartbox.com/public_html/t2/'
+const remoteCss = remoteFolder + 'css/'
+const remoteJs = remoteFolder + 'js/'
+const remoteTemplateParts = remoteFolder + 'template-parts/'
 
-const localFolder           = ''
-const localCss              = localFolder + 'css/'
-const localJs               = localFolder + 'js/'
-const localTemplateParts    = localFolder + 'template-parts/'
-
+const localFolder = ''
+const localCss = localFolder + 'css/'
+const localJs = localFolder + 'js/'
+const localTemplateParts = localFolder + 'template-parts/'
 
 
 function getFtpConnection() {
 	return ftp.create({
-		host:           host,
-		log:            gutil.log,
-		password:       password,
-		parallel:       3,
-		port:           port,
-		user:           user
+		host: host,
+		log: gutil.log,
+		password: password,
+		parallel: 3,
+		port: port,
+		user: user
 	})
 }
 
 let conn = getFtpConnection()
 
-
-
 gulp.task('css', function () {
-	return gulp.src(localCss + '_styles.scss')
+	return gulp.src(localCss + 'styles.scss')
 		.pipe(sass())
 		.pipe(cssMinify())
 		.pipe(rename({
@@ -54,7 +51,7 @@ gulp.task('css', function () {
 
 gulp.task('copy_css', function () {
 	return gulp.src(localCss + '**/*')
-		.pipe(conn.dest(remoteFolderCss))
+		.pipe(conn.dest(remoteCss))
 })
 
 gulp.task('copy_html', function () {
@@ -69,16 +66,16 @@ gulp.task('copy_template_parts', function () {
 
 gulp.task('copy_js', function () {
 	return gulp.src(localJs + '**/*.js')
-		.pipe(conn.dest(remoteFolderJs))
+		.pipe(conn.dest(remoteJs))
 })
 
 gulp.task('js', function () {
 	return gulp.src([
-			localJs + 'jquery.3.2.1.js',
-			localJs + 'owl.carousel.js',
-			localJs + 'jspdf.min.js',
-			localJs + '**/*.js'
-		])
+		localJs + 'jquery-3.7.1.min.js',
+		// localJs + 'owl.carousel.js',
+		// localJs + 'jspdf.min.js',
+		localJs + '**/*.js'
+	])
 		.pipe(concat('all.js'))
 		// .pipe(uglify())
 		.pipe(rename({
@@ -87,11 +84,11 @@ gulp.task('js', function () {
 		.pipe(conn.dest(remoteFolder))
 })
 
-gulp.task('watch', function() {
-	gulp.watch(localFolder + '*.php',           gulp.series('copy_html'))
-	gulp.watch(localCss + '**/*',               gulp.series('css', 'copy_css'))
-	gulp.watch(localJs + '**/*.js',             gulp.series('js', 'copy_js'))
-	gulp.watch(localTemplateParts + '**/*',     gulp.series('copy_template_parts'))
+gulp.task('watch', function () {
+	gulp.watch(localFolder + '*.php', gulp.series('copy_html'))
+	gulp.watch(localCss + '**/*', gulp.series('css', 'copy_css'))
+	gulp.watch(localJs + '**/*.js', gulp.series('js', 'copy_js'))
+	gulp.watch(localTemplateParts + '**/*', gulp.series('copy_template_parts'))
 })
 
 gulp.task('default', gulp.series('watch'))
---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+	<link rel="stylesheet" href="styles.min.css?v<?= (date("YmdHis")); ?>">
+
+	<title>Serves</title>
+</head>
+
+<body>
+<div class="hero">
+	<span class="logo js-img-scroll" data-src="i/logo.svg"></span>
+
+	<div class="hero__bg"></div>
+
+	<div class="hero__content">
+		<h1 class="hero__title">SERVS</h1>
+
+		<div class="hero__desc">флагманский поставщик серверов и комплектующих в России</div>
+	</div>
+
+	<div class="hero__more">
+		<button class="btn btn--primary">Смотреть каталог</button>
+	</div>
+</div>
+
+<script src="all.min.js?v<?= (date("YmdHis")); ?>"></script>
+</body>
+</html>
